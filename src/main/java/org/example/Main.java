b5e8aef79081373204aadd4d1e56f07f3bc62e1d
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-07-15 07:58:47 -0300

    Exemplo com debugg - step into, step over e step out

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,17 @@
 package org.example;
 
-import com.formdev.flatlaf.FlatDarculaLaf;
-import com.formdev.flatlaf.FlatDarkLaf;
-import com.formdev.flatlaf.FlatIntelliJLaf;
-import com.formdev.flatlaf.FlatLightLaf;
-import com.formdev.flatlaf.themes.FlatMacLightLaf;
-import org.example.view.TelaExemplo;
-
-import javax.swing.*;
-
 public class Main {
     static void main() {
 
-        try{
-            UIManager.setLookAndFeel(new FlatDarkLaf());
-
-            TelaExemplo telaExemplo = new TelaExemplo();
-            telaExemplo.pack();
-            telaExemplo.setVisible(true);
-            System.exit(0);
-
-        } catch (UnsupportedLookAndFeelException e) {
-            throw new RuntimeException(e);
-        }
+        int a = 10;
+        int b = 20;
+        int soma = soma(a,b);
+        System.out.println(soma);
 
     }
+
+    static int soma(int a, int b){
+        return a+b;
+    }
+
 }
