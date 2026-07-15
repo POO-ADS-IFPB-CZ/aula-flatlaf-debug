c1f2b23b038bc49920ba637cb5f0ac18738080a2
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-07-15 07:48:03 -0300

    Criando exemplo com flatlaf

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,28 @@
 package org.example;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
 import org.example.view.TelaExemplo;
+
+import javax.swing.*;
 
 public class Main {
     static void main() {
 
-        TelaExemplo telaExemplo = new TelaExemplo();
-        telaExemplo.pack();
-        telaExemplo.setVisible(true);
-        System.exit(0);
+        try{
+            UIManager.setLookAndFeel(new FlatMacLightLaf());
+
+            TelaExemplo telaExemplo = new TelaExemplo();
+            telaExemplo.pack();
+            telaExemplo.setVisible(true);
+            System.exit(0);
+
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
